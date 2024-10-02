149a4cf4563e9e234b8168d13b912f326bc8ce65
Halász Gergő (SZF_2023_2025) <halasz.gergo1@diak.szamalk-szalezi.hu>
2024-10-02 15:58:44 +0200

    Ajto nyitas!

--- src/ajtoFeladat/AjtoGUI.java
+++ src/ajtoFeladat/AjtoGUI.java
@@ -26,20 +26,141 @@ public class AjtoGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        lblCim = new javax.swing.JLabel();
+        lblCserevel = new javax.swing.JLabel();
+        SpnCserevel = new javax.swing.JSpinner();
+        lblMasodikAjto = new javax.swing.JLabel();
+        lblHarmadikAjto = new javax.swing.JLabel();
+        lblElsoAjto = new javax.swing.JLabel();
+        btnElsoGomb = new javax.swing.JButton();
+        btnMasodikGomb = new javax.swing.JButton();
+        btnHarmadikGomb = new javax.swing.JButton();
+        lblCsereNelkul = new javax.swing.JLabel();
+        SpnCsereNelkul = new javax.swing.JSpinner();
+        btnUjJatek = new javax.swing.JButton();
+        btnJatekVege = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jLabel3.setText("Kérlek, válassz egy ajtót!");
+
+        jLabel4.setText("A három közül, az egyik ajtó egy autót rejt!");
+
+        lblCim.setFont(new java.awt.Font("Arial", 0, 36)); // NOI18N
+        lblCim.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblCim.setText("Ajtó nyitás!");
+
+        lblCserevel.setText("Nyert körök cserével:");
+
+        lblMasodikAjto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ajtoFeladat/kepek/kicsi_11zon.png"))); // NOI18N
+
+        lblHarmadikAjto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ajtoFeladat/kepek/kicsi_11zon.png"))); // NOI18N
+
+        lblElsoAjto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ajtoFeladat/kepek/kicsi_11zon.png"))); // NOI18N
+
+        btnElsoGomb.setText("Ezt nyitom!");
+
+        btnMasodikGomb.setText("Ezt nyitom!");
+
+        btnHarmadikGomb.setText("Ezt nyitom!");
+
+        lblCsereNelkul.setText("Nyert körök csere nélkül:");
+
+        btnUjJatek.setText("Új játék!");
+        btnUjJatek.setMaximumSize(new java.awt.Dimension(102, 23));
+        btnUjJatek.setMinimumSize(new java.awt.Dimension(102, 23));
+        btnUjJatek.setPreferredSize(new java.awt.Dimension(102, 23));
+
+        btnJatekVege.setText("Abbahagyom");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 705, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(579, 579, 579)
+                        .addComponent(btnUjJatek, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblCserevel)
+                                .addGap(26, 26, 26)
+                                .addComponent(SpnCserevel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(56, 56, 56)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(6, 6, 6)
+                                        .addComponent(btnElsoGomb))
+                                    .addComponent(lblElsoAjto)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(lblCsereNelkul)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(SpnCsereNelkul, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(46, 46, 46)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel3)
+                            .addComponent(lblCim, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblMasodikAjto)
+                            .addComponent(btnMasodikGomb))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(81, 81, 81)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblHarmadikAjto)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(6, 6, 6)
+                                        .addComponent(btnHarmadikGomb))))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(79, 79, 79)
+                                .addComponent(btnJatekVege, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
+                .addGap(67, 67, 67))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 415, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblCim)
+                    .addComponent(lblCserevel)
+                    .addComponent(SpnCserevel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnUjJatek, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(btnJatekVege))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblCsereNelkul)
+                            .addComponent(SpnCsereNelkul, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lblMasodikAjto)
+                    .addComponent(lblHarmadikAjto)
+                    .addComponent(lblElsoAjto))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnMasodikGomb)
+                    .addComponent(btnElsoGomb)
+                    .addComponent(btnHarmadikGomb))
+                .addContainerGap(44, Short.MAX_VALUE))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
@@ -78,5 +199,20 @@ public class AjtoGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JSpinner SpnCsereNelkul;
+    private javax.swing.JSpinner SpnCserevel;
+    private javax.swing.JButton btnElsoGomb;
+    private javax.swing.JButton btnHarmadikGomb;
+    private javax.swing.JButton btnJatekVege;
+    private javax.swing.JButton btnMasodikGomb;
+    private javax.swing.JButton btnUjJatek;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel lblCim;
+    private javax.swing.JLabel lblCsereNelkul;
+    private javax.swing.JLabel lblCserevel;
+    private javax.swing.JLabel lblElsoAjto;
+    private javax.swing.JLabel lblHarmadikAjto;
+    private javax.swing.JLabel lblMasodikAjto;
     // End of variables declaration//GEN-END:variables
 }
